07577fa17df800e04901e3b2df559d059f0d519a
Isaac Yung Lopes <isaaczoi@gmail.com>
2016-12-21 20:46:01 -0200

    Bug with user agent

--- crawler-site.js
+++ crawler-site.js
@@ -1,23 +1,53 @@
 var Crawler = require("simplecrawler");
 
-var crawler = Crawler("http://alien-code.com/")
-.on("fetchcomplete", function () {
-});
-
-//Configuration
-crawler.interval = 500; // Ten seconds
-crawler.maxConcurrency = 10;
-crawler.maxDepth = 2;
-crawler.respectRobotsTxt = true;
-crawler.userAgent = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19";
-
-crawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
-  console.log("url: %s | %s (%d bytes)", queueItem.url, response.headers['content-type'], responseBuffer.length);
-});
-
-crawler.start();
-
-var siteCrawl = function(crawler) {
+var SiteCrawl = function(site) {
+  this.crawler = Crawler(site);
 };
 
-module.exports = siteCrawl;
+SiteCrawl.prototype.config = function(configs) {
+  this.crawler.interval = configs['interval'] || 200; // Ten seconds
+  this.crawler.maxConcurrency = configs['maxConcurrency'] || 8;
+  this.crawler.maxDepth = 3;
+  this.crawler.respectRobotsTxt = true;
+  this.crawler.userAgent = configs['userAgent'];
+}
+
+SiteCrawl.prototype.start = function() {
+  var countPages = 0;
+  var countNoCached = 0;
+  var cachedPrecentage = 0.0;
+
+  //this.crawler.userAgent = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19 Warmer";
+
+  this.crawler.addFetchCondition(function(parsedURL) {
+      if (parsedURL.path.match(/\.(css|jpg|pdf|docx|js|png|ico)/i)) {
+          return false;
+      }
+
+      return true;
+  });
+
+  this.crawler.on("complete", function(e) {
+
+  });
+
+  this.crawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
+    countPages++;
+    cache = ["MISS", "EXPIRED"];
+    if(cache.indexOf(response.headers['cf-cache-status']) > -1) {
+      countNoCached++;
+    }
+    cachedPrecentage = (countNoCached/countPages)*100;
+    console.log("%d %s > cold: %d% | url: %s | cache: %s", countPages,
+                                                           response.headers['markup-device'],
+                                                           cachedPrecentage.toFixed(2),
+                                                           queueItem.url,
+                                                           response.headers['cf-cache-status']
+    );
+  });
+
+  return this.crawler.start();
+}
+
+//Configuration
+module.exports = SiteCrawl;
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-var app = require('./crawler-site.js');
+var Crawler = require('./crawler-site.js');
+
+var crawlerLarge = crawlerMobile = new Crawler("https://www.bidu.com.br/");
+var userAgent = {
+  large: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36 Warmer",
+  mobile: "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19 Warmer"
+};
+
+
+crawlerLarge.config({
+  userAgent: userAgent['large']
+});
+
+crawlerMobile.config({
+  userAgent: userAgent['mobile']
+});
+
+crawlerLarge.start();
+
 
 console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
-  app();
+  console.log();
 
   callback(null, `Successfully processed ${event.key1} records.`);
 }
